e7f5c8222913fa45f2487db7fff9d7dd3d416af1
tiger-ff <wangpf321123@gmail.com>
2020-10-25 17:54:23 +0800

    commit

---
+++ src/main/java/com/tiger/springboot/action/controller/HelloController.java
@@ -0,0 +1,18 @@
+package com.tiger.springboot.action.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author tigerff
+ * @version 1.0
+ * @date 2020/10/25 17:17
+ */
+@RestController
+public class HelloController {
+    @GetMapping("/")
+    public String getHello()
+    {
+        return "成功了";
+    }
+}
